a11c202cfab9779b57cad7ac602a27b65d77d9a0
Maxwell Alexius <destinydragon85614@gmail.com>
2019-09-10 18:06:27 +0800

    Create a simple server using Express

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+import 'reflect-metadata';
+import express from 'express';
+
+const app = express();
+
+app.get('/', function(request, response) {
+  console.log('Hello World!');
+});
+
+console.log('Server listening on port: 8080...');
+app.listen(8080);
